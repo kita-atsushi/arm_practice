2a1bfdecbf33d7c7756bd5ef9ac019926e21b1f3
kita-atsushi <atsushi.kita1985@gmail.com>
2020-02-24 10:51:24 +0900

    First commit

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "bento/ubuntu-18.04"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = false
+    vb.memory = "1024"
+  end
+
+  config.vm.provision "shell", inline: <<-SHELL
+    apt-get update
+    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+  SHELL
+end
---
+++ tools/set_webapps_diagnostics.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+CWD="$(cd $(dirname $0) && pwd)"
+TARGET_WEBAPP_RESOURCE_ID=$1
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <TARGET_WEBAPP_RESOURCE_ID>"
+  exit 0
+fi
+LOG_ANALYTICS_WS_RG_NAME="testARMtemplate001"
+LOG_ANALYTICS_WS="testLogAnalytics2020022401"
+DIAGNOSTIC_NAME="ibsharediagnostic"
+ROLE="webapps"
+
+function replace_config_item() {
+  local TEMPLATE_FILEPATH=$1
+  local ITEM_NAME=$2
+  local ITEM_VALUE=$3
+  sed -i "${TEMPLATE_FILEPATH}" -e "s/@@IS_${ITEM_NAME}@@/${ITEM_VALUE}/g"
+}
+
+cd ${CWD}/target/${ROLE}
+cp template_logs.txt settings_logs.txt
+LOG_ITEMS=(
+  "AppServiceHTTPLogs"
+  "AppServiceConsoleLogs"
+  "AppServiceAppLogs"
+  "AppServiceFileAuditLogs"
+  "AppServiceAuditLogs"
+)
+for ele in ${LOG_ITEMS[@]}; do
+    replace_config_item "settings_logs.txt" "${ele}" "true"
+done
+
+cp template_metrics.txt settings_metrics.txt
+METRICS_ITEMS=(
+  "AllMetrics"
+)
+for ele in ${METRICS_ITEMS[@]}; do
+    replace_config_item "settings_metrics.txt" "${ele}" "true"
+done
+
+az monitor diagnostic-settings create \
+  --name "${DIAGNOSTIC_NAME}" \
+  --resource "${TARGET_WEBAPP_RESOURCE_ID}" \
+  --workspace "${LOG_ANALYTICS_WS}" \
+  --resource-group "${LOG_ANALYTICS_WS_RG_NAME}" \
+  --logs @settings_logs.txt \
+  --metrics @settings_metrics.txt
+
+rm -f settings_logs.txt settings_metrics.txt
+
+exit 0
